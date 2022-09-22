12edd2360ff6d9a6485f9333ff04b0abbd7feb68
Kiran Randhawa Kukar <Kiran.Randhawa-Kukar@softwire.com>
2022-09-22 11:26:58 +0100

    set up resources in routes for books

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'books/index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'books#index'
 
-  get '/books/:id', to: 'books#show'
+  resources :books
 
   # Defines the root path route ("/")
   # root "articles#index"
